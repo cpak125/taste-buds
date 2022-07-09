5e47a326373a0304a39c62e127a7891cb7e8c1c1
cpak125 <chris.pak125@gmail.com>
2022-07-08 21:06:57 -0400

    Create JWT token

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,13 +1,35 @@
+const jwt = require('jsonwebtoken');
+const User = require('../../models/user');
+
 module.exports = {
   create,
 };
 
-function create(req, res) {
-  // Baby step...
-  res.json({
-    user: {
-      name: req.body.name,
-      email: req.body.email
-    }
-  });
+async function create(req, res) {
+  try {
+    // Add the user to the database
+    const user = await User.create(req.body);
+    // token will be a string
+    const token = createJWT(user);
+    // Yes, we can serialize a string
+    res.json(token);
+  } catch (err) {
+    // Client will check for non-2xx status code
+    // 400 = Bad request
+    res.status(400).json(err);
+  }
+}
+
+
+/* Helper Functions */
+
+function createJWT(user) {
+  return jwt.sign(
+    // data payload
+    { user },
+    process.env.SECRET,
+    { expiresIn: '24h' }
+  );
+
+
 }
