b0ddd208cc37bffa9199e5762591051522dc97c0
cpak125 <chris.pak125@gmail.com>
2022-08-15 14:22:48 -0400

    Add some styling

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -49,13 +49,13 @@ export default class SignUpForm extends Component {
         <div className='SignUpForm'>
           <div className="form-container">
             <form autoComplete="off" onSubmit={this.handleSubmit}>
-              <label>Name</label>
+              <label>Name:</label>
               <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
-              <label>Email</label>
+              <label>Email:</label>
               <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
-              <label>Password</label>
+              <label>Password:</label>
               <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
-              <label>Confirm</label>
+              <label>Confirm:</label>
               <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
               <button type="submit" disabled={disable}>SIGN UP</button>
             </form>
